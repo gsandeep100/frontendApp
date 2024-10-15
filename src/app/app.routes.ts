4b8481e9f6ffc6f9884465f2be03642851d586b1
Sandeep Ghosh <gsandeep100@gmail.com>
2024-10-15 21:28:56 +0100

    Added Functionality

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {LoginComponent} from './components/login/login.component';
-
 export const routes: Routes = [
   { path: '', component: LoginComponent }, // Default route
 ];
